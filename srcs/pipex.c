c48a5a61ac1b4a5791cd393dfe43af0ceca03575
tokochiz <tokohana.1003@gmail.com>
2024-02-22 19:05:19 +0900

    :sparkles:add : 全体動作の概要を追加

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -6,49 +6,59 @@
 /*   By:  ctokoyod < ctokoyod@student.42tokyo.jp    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/17 17:19:21 by  ctokoyod         #+#    #+#             */
-/*   Updated: 2024/02/17 18:33:40 by  ctokoyod        ###   ########.fr       */
+/*   Updated: 2024/02/22 18:34:02 by  ctokoyod        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../include/pipex.h"
-
-/*
-ボーナスを作りたい場合は、再帰で多段パイプを実装
-*/
-
-// TODO :引数の解析
-// TODO :ファイルオープンとリダイレクション
-// TODO :パイプの作成
-// TODO :プロセスのフォーク？
-// TODO :実行とクリーンアップ
-
-// TODO :ボーナス　再帰で多段パイプを実装　複数パイプ処理関数
-// TODO :ボーナス　ヒアドキュメント処理関数作成する　here_doc の場合の処理
+#include "../includes/pipex.h"
 
 
-
-
-int	main(int argc, char **argv)
+// 環境変数へのアクセスが必要な場合 char *envp[]を引数に追加
+int	main(int argc, char **argv, char *envp[])
 {
-	// TODO(1) : 引数受け取り　
-	// TODO(1)-1 : 引数の数をカウントする→５つ必要
+	t_pipex	pipex;
+
+	// 引数チェック
 	if (argc != 5)
-	{
-		fprintf(stderr, "Usage : %s file1 cmd1 cmd2 file2\n", argv[0]);
-		return (1);
-	}
-	//  引数からファイル名とコマンドを取得
-	char *file1;
-	char *cmd1;
-	char *cmd2;
-	char *file2;
-	file1 = argv[1];
-	cmd1 = argv[2];
-	cmd2 = argv[3];
-	file2 = argv[4];
+		return (error_msg("Invalid number of arguments"));
+			// TODO: ERROR msg を出力させる
+			
+	// ファイルオープン
+	pipex.infile = open(argv[1], O_RDONLY); // 読み取り専用でファイルを開く
+	if (pipex.infile < 0)
+		error_msg(); // TODO: ERROR msg を出力させる
+		
+	pipex.outfile = open(argv[argc - 1], O_RDWR, ); //読み書きできるように権限を設定する
+	if (pipex.outfile < 0) 
+		error_msg();        // TODO: ERROR msg を出力させる
+		
+	// パイプ作成
+	pipe(pipex.tube);
+	if (pipex.tube < 0)
+		error_mgs();
+		
+	// コマンドパスの検索
+	pipex.paths = find_path_from_env(envp);
+	pipex.cmd_paths = ft_split(pipex.paths, ':');
+	
+	// ２つの子プロセスの作成
+	pipex.pid1 = fork();
+	if (pipex.pid1 == 0) //成功した場合、親プロセスには子プロセスの PID ,子プロセスには 0 が返る
+		first_child_process();
+	pipex.pid2 = fork();
+	if (pipex.pid2 == 0)
+		second_child_process();
 
-	// TODO : pipex関数に渡す
-	pipex(file1, cmd1, cmd2, file2);
+	// パイプのクローズ
+	close_pipes(&pipex);
 
+	
+	// 子プロセスの終了をまつ
+	wait_pid();
+	wait_pid();
+	
+	// 親リソースの解放
+	releace_parent_resource();
+	
 	return (0);
 }
