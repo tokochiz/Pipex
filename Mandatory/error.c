362628d39909cac3cd1592093d5b44ba1462c728
tokochiz <tokohana.1003@gmail.com>
2024-03-05 20:36:59 +0900

    fix : 関数名変更

--- Mandatory/error.c
+++ Mandatory/error.c
@@ -6,7 +6,7 @@
 /*   By:  ctokoyod < ctokoyod@student.42tokyo.jp    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 21:18:46 by  ctokoyod         #+#    #+#             */
-/*   Updated: 2024/03/01 16:20:49 by  ctokoyod        ###   ########.fr       */
+/*   Updated: 2024/03/02 16:49:26 by  ctokoyod        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,10 +29,3 @@ void	put_error_after_exit(const char *msg, int is_manual)
 		perror(msg);
 	exit(EXIT_FAILURE);
 }
-// void put_error_msg(const char *msg, int is_manual)
-// {
-// 	if (is_manual == 1)
-// 		ft_putstr_fd(msg, STDERR_FILENO);
-// 	else
-// 		perror(msg);
-// }
